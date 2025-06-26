2c5f0e4fcd547a0852a438f2e8648fe0efcdfc3f
KilJae69 <adi.toromanovic@outlook.com>
2025-06-26 16:26:23 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import BestSellersSection from "@/components/home-sections/BestSellersSection";
 import CategoriesSection from "@/components/home-sections/CategoriesSection";
 // import FeaturedBrandsSection from "@/components/home-sections/FeaturedBrandsSection";
 import HeroSlider from "@/components/home-sections/HeroSlider";
-import MarqueeSection from "@/components/home-sections/MarqueeSection";
+// import MarqueeSection from "@/components/home-sections/MarqueeSection";
 
 import GlareCTA from "@/components/shared/GlareCTA";
 // import { Container } from "@/components/shared/Container";
@@ -14,7 +14,7 @@ export default function HomePage() {
     <>
       <HeroSlider />
       {/* <FeaturedBrandsSection /> */}
-      <MarqueeSection/>
+      {/* <MarqueeSection/> */}
       <CategoriesSection/>
       <BestSellersSection/>
         <GlareCTA />
