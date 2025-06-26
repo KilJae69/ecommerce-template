7267c6e182c49f88635dd4c395f5cb8d25cba2d5
KilJae69 <adi.toromanovic@outlook.com>
2025-06-26 16:40:18 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 // src/components/pages/home-page.tsx
 
-// import BestSellersSection from "@/components/home-sections/BestSellersSection";
-// import CategoriesSection from "@/components/home-sections/CategoriesSection";
-// import FeaturedBrandsSection from "@/components/home-sections/FeaturedBrandsSection";
-// import HeroSlider from "@/components/home-sections/HeroSlider";
-// import MarqueeSection from "@/components/home-sections/MarqueeSection";
+ import BestSellersSection from "@/components/home-sections/BestSellersSection";
+ import CategoriesSection from "@/components/home-sections/CategoriesSection";
+ import FeaturedBrandsSection from "@/components/home-sections/FeaturedBrandsSection";
+ import HeroSlider from "@/components/home-sections/HeroSlider";
+ import MarqueeSection from "@/components/home-sections/MarqueeSection";
 
-// import GlareCTA from "@/components/shared/GlareCTA";
-// import { Container } from "@/components/shared/Container";
+ import GlareCTA from "@/components/shared/GlareCTA";
+
 
 export default function HomePage() {
   return (
     <>
-      {/* <HeroSlider /> */}
-      {/* <FeaturedBrandsSection /> */}
-      {/* <MarqueeSection/> */}
-      {/* <CategoriesSection/> */}
-      {/* <BestSellersSection/> */}
-        {/* <GlareCTA /> */}
+      <HeroSlider /> 
+      <FeaturedBrandsSection /> 
+      <MarqueeSection/> 
+      <CategoriesSection/> 
+       <BestSellersSection/> 
+         <GlareCTA /> 
      
     </>
   );
