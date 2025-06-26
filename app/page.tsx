b47031120a360fd33a88b0767bc63c4132793410
KilJae69 <adi.toromanovic@outlook.com>
2025-06-26 16:28:53 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 // src/components/pages/home-page.tsx
 
-import BestSellersSection from "@/components/home-sections/BestSellersSection";
-import CategoriesSection from "@/components/home-sections/CategoriesSection";
+// import BestSellersSection from "@/components/home-sections/BestSellersSection";
+// import CategoriesSection from "@/components/home-sections/CategoriesSection";
 // import FeaturedBrandsSection from "@/components/home-sections/FeaturedBrandsSection";
 import HeroSlider from "@/components/home-sections/HeroSlider";
 // import MarqueeSection from "@/components/home-sections/MarqueeSection";
 
-import GlareCTA from "@/components/shared/GlareCTA";
+// import GlareCTA from "@/components/shared/GlareCTA";
 // import { Container } from "@/components/shared/Container";
 
 export default function HomePage() {
@@ -15,9 +15,9 @@ export default function HomePage() {
       <HeroSlider />
       {/* <FeaturedBrandsSection /> */}
       {/* <MarqueeSection/> */}
-      <CategoriesSection/>
-      <BestSellersSection/>
-        <GlareCTA />
+      {/* <CategoriesSection/> */}
+      {/* <BestSellersSection/> */}
+        {/* <GlareCTA /> */}
      
     </>
   );
